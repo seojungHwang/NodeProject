61b92aaa9604ee223328f0fff1bdf979a99c6e9b
seojungHwang <tjwjd101@gmail.com>
2022-01-10 22:13:01 +0900

    [add] sns-service project

---
+++ sns-service/app.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+const path = require('path');
+const session = require('express-session');
+const nunjucks = require('nunjucks');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const pageRouter = require('./routes/page');
+const {sequelize} = require('./models');
+const console = require('console');
+
+const app = express();
+app.set('port', process.env.PORT || 8001);
+app.set('view engine', 'html');
+nunjucks.configure('views', {
+  express: app,
+  watch: true,
+});
+sequelize.sync({force:false})  //alter:true -> 기존 데이터와 컬럼이 안맞아서 에러 날 수 있음(데이터 안지워짐)/ force:true -> 테이블이 지워졌다가 다시 생성되지만 데이터도 지워짐
+    .then(() => {
+        console.log('데이터베이스 연결 성공');
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public'))); //정적파일 css
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(session({
+  resave: false,
+  saveUninitialized: false,
+  secret: process.env.COOKIE_SECRET,
+  cookie: {
+    httpOnly: true,
+    secure: false,
+  },
+}));
+
+app.use('/', pageRouter);
+
+//404처리 미들웨어
+app.use((req, res, next) => {
+  const error =  new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  next(error);
+});
+
+//에러 미들웨어
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+  res.status(err.status || 500).render('error');
+});
+
+app.listen(app.get('port'), () => {
+  console.log(app.get('port'), '번 포트에서 대기중');
+});
